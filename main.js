f99b52a7d1f98ad152d09a91206e66302789400d
umbrellamans <dqsjtdw@163.com>
2017-05-19 20:26:14 +0800

    init commit

---
+++ main.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ * @authors Your Name (you@example.org)
+ * @date    2017-05-19 17:55:30
+ * @version $Id$
+ */
+
+$(document).ready(function(){
+
+	$("div.mainvisual_list_in:eq(1)").hide();
+	$("div.mainvisual_list_in:eq(2)").hide();
+});
